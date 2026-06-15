126f44086ee675f6896bede908a19f99f95a4942
Daniiel2006 <danieldiagosanchez@gmail.com>
2026-06-15 22:31:14 +0100

    Add files via upload

--- Backend/TravelGo/TravelGo/settings.py
+++ Backend/TravelGo/TravelGo/settings.py
@@ -128,7 +128,10 @@ REST_FRAMEWORK = {
 }
 
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+    "https://travelgo-proyecto-1.onrender.com",
+]
 
 import os
 
